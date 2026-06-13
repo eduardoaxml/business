134c5a5a45ff97d42efdd2f8933fa472d56d567b
Pablo Eduardo Merino <3802987+eduardoaxml@users.noreply.github.com>
2026-06-13 15:49:44 -0600

    State management and Handling events

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
+
 type Props = {
   data: string[];
 };
 
 const List = ({ data }: Props) => {
+  const [index, setIndex] = useState(0);
+  const handleClick = (i: number) => {
+    setIndex(i);
+  };
   return (
     <div>
       <ul className="list-group">
-        {data.map((item) => (
-          <li key={item} className="list-group-item">
+        {data.map((item, i) => (
+          <li
+            onClick={() => handleClick(i)}
+            key={item}
+            className={`list-group-item ${index == i ? "active" : ""}`}
+          >
             {item}
           </li>
         ))}
